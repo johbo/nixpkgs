61fc9d704da84ced92254f1582d391580cf993b4
Johannes Bornhold <johannes@bornhold.name>
2016-12-25 00:56:49 +0100

    gtksourceview: Add gtk-mac-integration for darwin
    Had to pick two commits as patches from the repository which update the
    dependency ige-mac-integration to gtk-mac-integration.

--- pkgs/desktops/gnome-2/desktop/gtksourceview/default.nix
+++ pkgs/desktops/gnome-2/desktop/gtksourceview/default.nix
@@ -1,5 +1,8 @@
-{stdenv, fetchurl, pkgconfig, atk, cairo, glib, gtk, pango, 
-  libxml2Python, perl, intltool, gettext}:
+{stdenv, fetchurl, autoreconfHook, pkgconfig, atk, cairo, glib
+, gnome_common, gtk, pango
+, libxml2Python, perl, intltool, gettext, gtk-mac-integration }:
+
+with stdenv.lib;
 
 stdenv.mkDerivation rec {
   name = "gtksourceview-${version}";
@@ -9,6 +12,21 @@ stdenv.mkDerivation rec {
     url = "mirror://gnome/sources/gtksourceview/2.10/${name}.tar.bz2";
     sha256 = "c585773743b1df8a04b1be7f7d90eecdf22681490d6810be54c81a7ae152191e";
   };
-  buildInputs = [pkgconfig atk cairo glib gtk pango libxml2Python perl intltool
-    gettext];
+
+  patches = optionals stdenv.isDarwin [
+    ./0001-Change-IgeMacIntegration-to-GtkOSXApplication.patch
+    ./0002-Update-to-gtk-mac-integration-2.0-API.patch
+  ];
+
+  buildInputs = [
+    pkgconfig atk cairo glib gtk
+    pango libxml2Python perl intltool
+    gettext
+  ] ++ optionals stdenv.isDarwin [
+    autoreconfHook gnome_common gtk-mac-integration
+  ];
+
+  preConfigure = optionalString stdenv.isDarwin ''
+    intltoolize --force
+  '';
 }
